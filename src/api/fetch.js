c4af2c30b55a0790820e6fcacce9416553649869
JorgePeju <123939712+JorgePeju@users.noreply.github.com>
2023-05-17 11:34:31 +0200

    useForm y fetch hecho

---
+++ src/api/fetch.js
@@ -0,0 +1,34 @@
+export const consultation = async (url, method, body = {}) => {
+
+  let options = {};
+  const data = { ...body };
+
+  try {
+
+    if (method === 'POST' || method === 'PUT' ) {
+      
+      options = {
+        method,
+        body: JSON.stringify(data),
+        headers: {
+          'Content-type': 'application/json',
+        },
+      };
+    } 
+    
+    if (method === 'DELETE') {
+      options = { method };
+    }
+   
+    const request = await fetch(url, options);
+
+    if (request) {
+      return request.json();
+    } else {
+      throw new Error('Error al conectar con la api');
+    }
+
+  } catch (error) {
+    throw new Error(`Error al conectar con la API: ${error.message}`);
+  }
+};
