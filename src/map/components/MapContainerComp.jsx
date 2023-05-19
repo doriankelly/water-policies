84844704fd021050c988a1b21654ce99333afaca
santism98 <santiagosuarezmanso@gmail.com>
2023-05-19 13:52:05 +0200

    mapa funcional, falta decidir datos a mostrar

--- src/map/components/MapContainerComp.jsx
+++ src/map/components/MapContainerComp.jsx
@@ -1,19 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer} from "react-leaflet";
-import { Markers } from "./Markers";
 
 export const MapContainerComp = () => {
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://retoolapi.dev/BnvMF0/data");
+      const data = await response.json();
+      setMarkers(data);
+    };
+    fetchData();
+  }, []);
 
   return (
-    <>
-
-      <MapContainer center={[38.90892927891196, -6.617053521585592]} zoom={13} doubleClickZoom={false} attributionControl={false}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Markers/>
-      </MapContainer>
-
-    </>
-  )
-}
+    <MapContainer center={[42.53125302957343, -5.8680262289283505]} zoom={13} style={{ height: "500px" }}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {markers.map((marker) => (
+        <Marker key={marker.id} position={[marker.Lat, marker.Long]}>
+          <Popup>
+            Río: {marker.Embalse}
+            <br />
+            Tramo: {marker.Cuenca}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
--- src/map/components/index.js
+++ src/map/components/index.js
@@ -1,2 +1,2 @@
 export * from './MapContainerComp'
-export * from './Markers'
+// export * from './Markers'
