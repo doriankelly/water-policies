ed19b1b0b5d4b20974540406f7eb96d6ae9a36f4
dorian-rose <dorianrkelly@gmail.com>
2023-05-20 13:12:57 +0200

    final result card

--- src/questions/pages/FeedbackPage.jsx
+++ src/questions/pages/FeedbackPage.jsx
@@ -6,6 +6,7 @@ import { Feedback3 } from "../components/Feedback3";
 import { Feedback4 } from "../components/Feedback4";
 import { Feedback5 } from "../components/Feedback5";
 import { Link, useParams } from "react-router-dom";
+import { FinalFeedback } from "../components/FinalFeedback";
 
 export const FeedbackPage = () => {
   const { number } = useParams();
@@ -19,18 +20,28 @@ export const FeedbackPage = () => {
         {number == 3 && <Feedback3 />}
         {number == 4 && <Feedback4 />}
         {number == 5 && <Feedback5 />}
+        {number == 6 && <FinalFeedback />}
       </article>
       <img
         className="relative -top-32 block m-auto w-56"
         src={gota}
         alt="logo"
       />
-      <Link
-        to={`/quiz/${parseInt(number) + 1}`}
-        className="fixed left-1/2 -translate-x-1/2 bottom-2 drop-shadow w-11/12 bg-primary text-white hover:bg-secondary  block  mb-10 text-center m-auto py-2 shadow-lg rounded-3xl"
-      >
-        Siguiente
-      </Link>
+      {number == 5 ? (
+        <Link
+          to="/final"
+          className="fixed left-1/2 -translate-x-1/2 bottom-2 drop-shadow w-11/12 bg-primary text-white hover:bg-secondary  block  mb-10 text-center m-auto py-2 shadow-lg rounded-3xl"
+        >
+          Siguiente
+        </Link>
+      ) : (
+        <Link
+          to={`/quiz/${parseInt(number) + 1}`}
+          className="fixed left-1/2 -translate-x-1/2 bottom-2 drop-shadow w-11/12 bg-primary text-white hover:bg-secondary  block  mb-10 text-center m-auto py-2 shadow-lg rounded-3xl"
+        >
+          Siguiente
+        </Link>
+      )}
     </div>
   );
 };
