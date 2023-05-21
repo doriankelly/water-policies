de65a0f8b997dc38af4fd846661ffa6cb6e21f76
dorian-rose <dorianrkelly@gmail.com>
2023-05-21 12:13:44 +0200

    quiz results sent via fetch

--- src/questions/components/FinalFeedback.jsx
+++ src/questions/components/FinalFeedback.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
+import { consultation } from "../../api/fetch";
 
 export const FinalFeedback = () => {
-  console.log("Component rendered");
   const [userAnswers, setUserAnswers] = useState("");
   const [counter, setCounter] = useState(0);
   //collect current answers state
@@ -23,17 +23,27 @@ export const FinalFeedback = () => {
     question4: "option2",
   };
 
-  console.log("correct", answers);
   //calculate score
   useEffect(() => {
     for (const key in answers) {
-      console.log("user", userAnswers);
       if (userAnswers[key] === answers[key]) {
         setCounter((previousNumber) => previousNumber + 1);
       }
     }
   }, [userAnswers]);
 
-  console.log(counter);
+  //define arguments for fetch
+  const url = import.meta.env.VITE_RESULT_URL;
+  const method = "POST";
+  const body = {
+    questions: { ...answersObject },
+    status: { ...visitedObject },
+  };
+  console.log(body);
+  //call fetch to send result data
+  useEffect(() => {
+    consultation(url, method, body);
+  }, []);
+
   return <p>{`final result: Acertaste ${counter} preguntas!`}</p>;
 };
