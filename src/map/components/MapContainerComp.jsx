87baa91dc3b32bcaf4d2d9aa3321e3135488fe74
JorgePeju <123939712+JorgePeju@users.noreply.github.com>
2023-05-24 20:58:15 +0200

    icono de marcador nuevo

--- src/map/components/MapContainerComp.jsx
+++ src/map/components/MapContainerComp.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import newlogomarker from '../../assets/newlogomarker.png'
 
 export const MapContainerComp = () => {
   const [markers, setMarkers] = useState([]);
@@ -15,6 +16,13 @@ export const MapContainerComp = () => {
     fetchData();
   }, []);
 
+  const icon = L.icon({
+    iconSize: [20, 30],
+    iconAnchor: [20, 25],
+    popupAnchor: [-10, -25],
+    iconUrl: newlogomarker, 
+  })
+
   return (
     <MapContainer
       center={[42.38315517830405, -6.690562337456111]}
@@ -25,7 +33,7 @@ export const MapContainerComp = () => {
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       {markers.map((marker) => (
-        <Marker key={marker.id} position={[marker.Lat, marker.Long]}>
+        <Marker key={marker.id} position={[marker.Lat, marker.Long]} icon={icon}>
           <Popup>
             Embalse: {marker.Embalse}
             <br />
